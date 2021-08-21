b1752827827f9b1119ed9f7eae4ae5575bdf3d6c
tyler4400 <tyler4400@foxmail.com>
2021-08-21 16:28:11 +0800

    第一节 组件

--- src/page/component/index.js
+++ src/page/component/index.js
@@ -71,7 +71,7 @@ import './index.scss'
 //     componentDidMount(){
 //         console.log(this)
 //     }
-//     handerClick= (value) => console.log(value) 
+//     handerClick= (value) => console.log(value)
 //     handerChange=(value) => console.log(value)
 //     render(){
 //         return <div style={{ marginTop:'50px' }} >
@@ -94,7 +94,7 @@ import './index.scss'
 //     }
 // }
 // /* 函数 */
-// function textFun (){ 
+// function textFun (){
 //     return 'hello, world'
 // }
 // /* 函数组件 */
@@ -178,37 +178,37 @@ import './index.scss'
 // }
 
 // /* TODO:  继承 */
-// /* 人类 */
-// class Person extends React.Component{
-//     constructor(props){
-//         super(props)
-//         console.log('hello , i am person')
-//     }
-//     componentDidMount(){ console.log(1111)  }
-//     eat(){    /* 吃饭 */ }
-//     sleep(){  /* 睡觉 */  }
-//     ddd(){   console.log('打豆豆')  /* 打豆豆 */ }
-//     render(){
-//         return <div>
-//             大家好，我是一个person
-//         </div>
-//     }
-// }
-// /* 程序员 */
-// class programmer extends Person{
-//     constructor(props){
-//         super(props)
-//         console.log('hello , i am Programmer too')
-//     }
-//     componentDidMount(){  console.log(this)  }
-//     code(){ /* 敲代码 */ }
-//     render(){
-//         return <div style={ { marginTop:'50px' } } >
-//             { super.render() }
-//             我还是一个程序员！
-//         </div>
-//     }
-// }
+/* 人类 */
+class Person extends React.Component{
+    constructor(props){
+        super(props)
+        console.log('hello , i am person')
+    }
+    componentDidMount(){ console.log(1111)  }
+    eat(){    /* 吃饭 */ }
+    sleep(){  /* 睡觉 */  }
+    ddd(){   console.log('打豆豆')  /* 打豆豆 */ }
+    render(){
+        return <div>
+            大家好，我是一个person
+        </div>
+    }
+}
+/* 程序员 */
+class programmer extends Person{
+    constructor(props){
+        super(props)
+        console.log('hello , i am Programmer too')
+    }
+    componentDidMount(){  console.log(this)  }
+    code(){ /* 敲代码 */ }
+    render(){
+        return <div style={ { marginTop:'50px' } } >
+            { super.render() }
+            我还是一个程序员！
+        </div>
+    }
+}
 
 /* TODO: 组件插槽 */
 // const Children = (props)=> (<div>
